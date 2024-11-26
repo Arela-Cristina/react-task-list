24a1b9cae398d531f84d6f0e58dbd21dcc5a81a3
Arela-Cristina <instagrandine@gmail.com>
2024-11-26 15:44:15 +0100

    fix: error and create tasks structure

--- react-task-list/src/App.jsx
+++ react-task-list/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import viteLogo from "/vite.svg";
-import  tasksArray  from "../src/assets/tasks.js";
+import tasksArray from "../src/assets/tasks.js";
 import "./App.css";
 
 function App() {
@@ -11,7 +11,21 @@ function App() {
   return (
     <>
       {/* prova */}
-      <h1>prova</h1>
+      <header>
+        <h1>Task Manager</h1>
+      </header>
+
+      <section>
+        <h3>Current Task 4</h3>
+        <div>
+          <div>
+            {/* Task */}
+            {/* priority */}
+            {/* Est time */}
+          </div>
+          <div>{/* boton */}</div>
+        </div>
+      </section>
     </>
   );
 }
